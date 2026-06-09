ea516aa2d2e378b4d55df761936da68584afce62
yuutatamukai-lab <yuuta.tamukai@gmail.com>
2026-06-09 11:29:20 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,49 @@
+const CACHE_NAME = "pascucci-v1";
+const ASSETS_TO_CACHE = [
+  "/",
+  "/login.html",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
 self.addEventListener("install", event => {
-  console.log("Service Worker installed");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {});
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      return (
+        cached ||
+        fetch(event.request)
+          .then(response => {
+            const responseClone = response.clone();
+            caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, responseClone);
+            });
+            return response;
+          })
+          .catch(() => cached)
+      );
+    })
+  );
+});
